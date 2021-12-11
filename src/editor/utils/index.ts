c813923fe34fb721a43a608786e4e2266046a327
黄云飞 <huangyunfeihufe@hotmail.com>
2021-12-11 23:04:26 +0800

    feat:add lib mode build script

--- src/editor/utils/index.ts
+++ src/editor/utils/index.ts
@@ -1,8 +1,10 @@
 export function debounce(func: Function, delay: number) {
   let timer: number
   return function (...args: any) {
-    if (timer) clearTimeout(timer)
-    timer = setTimeout(() => {
+    if (timer) {
+      window.clearTimeout(timer)
+    }
+    timer = window.setTimeout(() => {
       // @ts-ignore
       func.apply(this, args)
     }, delay)
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
+import * as path from 'path'
 
-export default defineConfig({
-  base: '/canvas-editor/'
+export default defineConfig(({ mode }) => {
+  const name = 'canvas-editor'
+  const defaultOptions: UserConfig = {
+    base: `/${name}/`
+  }
+  if (mode === 'lib') {
+    return {
+      ...defaultOptions,
+      build: {
+        lib: {
+          name,
+          fileName: (format) => `${name}.${format}.js`,
+          entry: path.resolve(__dirname, 'src/editor/index.ts')
+        }
+      }
+    }
+  }
+  return {
+    ...defaultOptions
+  }
 })
