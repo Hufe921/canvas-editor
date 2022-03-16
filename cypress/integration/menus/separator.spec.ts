cfab32d2f2a6237fd96b13ae5d0135e9f0e316e4
Hufe921 <huangyunfeihufe@hotmail.com>
2022-03-16 20:09:24 +0800

    feat:add separator test case

---
+++ cypress/integration/menus/separator.spec.ts
@@ -0,0 +1,36 @@
+import Editor from '../../../src/editor'
+
+describe('菜单-分割线', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/canvas-editor/')
+
+    cy.get('canvas').first().as('canvas').should('have.length', 1)
+  })
+
+  it('分割线', () => {
+    cy.getEditor().then((editor: Editor) => {
+      editor.listener.saved = function (payload) {
+        const data = payload.data
+
+        expect(data[0].type).to.eq('separator')
+
+        expect(data[0]?.dashArray?.[0]).to.eq(1)
+
+        expect(data[0]?.dashArray?.[1]).to.eq(1)
+      }
+
+      editor.command.executeSelectAll()
+
+      editor.command.executeBackspace()
+
+      cy.get('.menu-item__separator').click()
+
+      cy.get('.menu-item__separator li').eq(1).click()
+        .then(() => {
+          cy.get('@canvas').type('{ctrl}s')
+        })
+    })
+  })
+
+})
