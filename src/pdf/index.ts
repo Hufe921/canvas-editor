561b212a418489c062693c3f53c50cc6980c1dbc
Hufe921 <huangyunfeihufe@hotmail.com>
2022-05-06 21:49:07 +0800

    feat:add font for export pdf

--- src/pdf/index.ts
+++ src/pdf/index.ts
@@ -2,18 +2,28 @@ import { IEditorResult } from '../editor'
 import { jsPDF } from 'jspdf'
 
 export const buildPdf = (payload: IEditorResult) => {
-  console.log('payload: ', payload)
-  const { width, height } = payload
+  const { width, height, data } = payload
   const doc = new jsPDF({
     orientation: 'p',
     unit: 'px',
     format: [width, height],
     hotfixes: ['px_scaling'],
+    compress: true
   })
+  // 添加字体
+  doc.addFont('/src/assets/font/msyh.ttf', 'Yahei', 'normal')
+  doc.setFont('Yahei')
+
+  // 上下文
   const ctx = doc.context2d
 
-  ctx.fillText('canvas-editor', 100, 100)
+  // 渲染
+  for (let d = 0; d < data.length; d++) {
+    const element = data[d]
+    ctx.fillText(element.value, 0, 0)
+  }
 
+  // 新页面显示
   const uri = doc.output('bloburi')
   window.open(uri, '_blank')
 }
