a4abaca5a7e3b7758989c078b260165e4f9aee22
黄云飞 <huangyunfeihufe@hotmail.com>
2022-09-29 16:33:07 +0800

    feat:optimize rich text render performance

--- src/editor/core/draw/richtext/Underline.ts
+++ src/editor/core/draw/richtext/Underline.ts
@@ -1,23 +1,29 @@
+import { AbstractRichText } from './AbstractRichText'
 import { IEditorOption } from '../../../interface/Editor'
 import { Draw } from '../Draw'
 
-export class Underline {
+export class Underline extends AbstractRichText {
 
   private options: Required<IEditorOption>
 
   constructor(draw: Draw) {
+    super()
     this.options = draw.getOptions()
   }
 
-  public render(ctx: CanvasRenderingContext2D, color: string, x: number, y: number, width: number) {
+  public render(ctx: CanvasRenderingContext2D) {
+    if (!this.fillRect.width) return
     const { underlineColor } = this.options
+    const { x, y, width } = this.fillRect
     ctx.save()
-    ctx.strokeStyle = color || underlineColor
+    ctx.strokeStyle = this.fillColor || underlineColor
+    const adjustY = y + 0.5 // 从1处渲染，避免线宽度等于3
     ctx.beginPath()
-    ctx.moveTo(x, y)
-    ctx.lineTo(x + width, y)
+    ctx.moveTo(x, adjustY)
+    ctx.lineTo(x + width, adjustY)
     ctx.stroke()
     ctx.restore()
+    this.clearFillInfo()
   }
 
 }
