debf30304b1fae8a4ec547ba977b4fad48328473
Hufe921 <huangyunfeihufe@hotmail.com>
2022-10-18 20:03:20 +0800

    fix:print svg editor

--- src/editor/utils/print.ts
+++ src/editor/utils/print.ts
@@ -1,15 +1,15 @@
-export function printImageBase64(base64List: string[], width: number, height: number) {
+export function printImageBase64(svgList: string[], width: number, height: number) {
   const iframe = document.createElement('iframe')
   document.body.append(iframe)
   const doc = iframe.contentWindow!.document
   doc.open()
   const container = document.createElement('div')
-  base64List.forEach(base64 => {
-    const image = document.createElement('img')
-    image.style.width = `${width}px`
-    image.style.height = `${height}px`
-    image.src = base64
-    container.append(image)
+  svgList.forEach(svg => {
+    const div = document.createElement('div')
+    div.style.width = `${width}px`
+    div.style.height = `${height}px`
+    div.innerHTML = svg
+    container.append(div)
   })
   const style = document.createElement('style')
   const stylesheet = `*{margin:0;padding:0;}`
