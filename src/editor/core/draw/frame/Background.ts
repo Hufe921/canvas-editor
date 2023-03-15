f076f2bd00eae6f3adf56559e40807898a8de229
Hufe921 <huangyunfeihufe@hotmail.com>
2023-03-15 20:05:00 +0800

    feat:adjust background when paper direction change

--- src/editor/core/draw/frame/Background.ts
+++ src/editor/core/draw/frame/Background.ts
@@ -1,16 +1,16 @@
-import { IEditorOption } from '../../../interface/Editor'
 import { Draw } from '../Draw'
 
 export class Background {
 
-  private options: Required<IEditorOption>
+  private draw: Draw
 
   constructor(draw: Draw) {
-    this.options = draw.getOptions()
+    this.draw = draw
   }
 
   public render(ctx: CanvasRenderingContext2D) {
-    const { width, height } = this.options
+    const width = this.draw.getOriginalWidth()
+    const height = this.draw.getOriginalHeight()
     ctx.save()
     ctx.fillStyle = '#ffffff'
     ctx.fillRect(0, 0, width, height)
