4773c0363ccb15b4ca9b406bb2c888a53c9af777
Hufe921 <huangyunfeihufe@hotmail.com>
2023-03-24 23:12:17 +0800

    chore: verify release package

--- scripts/release.js
+++ scripts/release.js
@@ -4,6 +4,11 @@ import path from 'path'
 
 const pkgPath = path.resolve('package.json')
 
+// 校验包合法性
+fs.accessSync(path.resolve('dist'), fs.constants.F_OK)
+fs.accessSync(path.resolve('dist/canvas-editor.es.js'), fs.constants.F_OK)
+fs.accessSync(path.resolve('dist/canvas-editor.umd.js'), fs.constants.F_OK)
+
 // 缓存项目package.json
 const sourcePkg = fs.readFileSync(pkgPath, 'utf-8')
 
