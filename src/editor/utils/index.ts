0a0ab44058e61c8f80eae00b81e155e33cd45967
TaroSunn <sunxiaomingyes@gmail.com>
2023-06-12 20:38:23 +0800

    types: 'this' issue in debounce function

--- src/editor/utils/index.ts
+++ src/editor/utils/index.ts
@@ -1,11 +1,10 @@
 export function debounce(func: Function, delay: number) {
   let timer: number
-  return function (...args: any) {
+  return function (this: any, ...args: any[]) {
     if (timer) {
       window.clearTimeout(timer)
     }
     timer = window.setTimeout(() => {
-      // @ts-ignore
       func.apply(this, args)
     }, delay)
   }
