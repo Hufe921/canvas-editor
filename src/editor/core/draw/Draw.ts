16c2e9a6d7d325aa33a8e8b5aec518f9e9d3861a
Captain <49203535+MssText@users.noreply.github.com>
2023-06-13 17:40:45 +0800

    fix: not allow change zone in continuous mode #201
    Co-authored-by: Hufe921 <huangyunfeihufe@hotmail.com>

--- src/editor/core/draw/Draw.ts
+++ src/editor/core/draw/Draw.ts
@@ -620,6 +620,7 @@ export class Draw {
       this._disconnectLazyRender()
       this.header.recovery()
       this.footer.recovery()
+      this.zone.setZone(EditorZone.MAIN)
     }
     this.render({
       isSubmitHistory: false,
