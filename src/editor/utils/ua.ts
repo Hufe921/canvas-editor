8cf2b19d3db2b48b840641c05156d1b3a0297b2b
Hufe921 <huangyunfeihufe@hotmail.com>
2023-09-23 22:14:19 +0800

    feat: typing on ios devices #286

--- src/editor/utils/ua.ts
+++ src/editor/utils/ua.ts
@@ -1,2 +1,5 @@
 export const isApple =
   typeof navigator !== 'undefined' && /Mac OS X/.test(navigator.userAgent)
+
+export const isIOS =
+  typeof navigator !== 'undefined' && /iPad|iPhone/.test(navigator.userAgent)
