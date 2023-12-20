d83fc0f37a368cb14b631ff10a4618789562d570
Hufe921 <huangyunfeihufe@hotmail.com>
2023-12-20 20:43:47 +0800

    chore: insert hyperlink with default value #368

--- src/main.ts
+++ src/main.ts
@@ -424,7 +424,8 @@ window.onload = function () {
           label: '文本',
           name: 'name',
           required: true,
-          placeholder: '请输入文本'
+          placeholder: '请输入文本',
+          value: instance.command.getRangeText()
         },
         {
           type: 'text',
