a96d2390365c6fe058e15b654bf5589373214109
baseWalker <104012756+baseWalker@users.noreply.github.com>
2024-01-11 17:53:15 +0800

    feat: support drop images #398 (#402)

--- src/editor/core/event/handlers/drop.ts
+++ src/editor/core/event/handlers/drop.ts
@@ -1,9 +1,19 @@
 import { CanvasEvent } from '../CanvasEvent'
+import { pasteImage } from './paste'
 
 export function drop(evt: DragEvent, host: CanvasEvent) {
   evt.preventDefault()
   const data = evt.dataTransfer?.getData('text')
   if (data) {
     host.input(data)
+  } else {
+    const files = evt.dataTransfer?.files
+    if (!files) return
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i]
+      if (file.type.startsWith('image')) {
+        pasteImage(host, file)
+      }
+    }
   }
 }
