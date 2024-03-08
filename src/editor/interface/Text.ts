245a77cd1fd550cd2ec59afd65d89ab7d2673718
Hufe921 <huangyunfeihufe@hotmail.com>
2024-03-08 18:15:55 +0800

    types: add ITextMetrics interface

--- src/editor/interface/Text.ts
+++ src/editor/interface/Text.ts
@@ -1,5 +1,15 @@
 import { TextDecorationStyle } from '../dataset/enum/Text'
 
+export interface ITextMetrics {
+  width: number
+  actualBoundingBoxAscent: number
+  actualBoundingBoxDescent: number
+  actualBoundingBoxLeft: number
+  actualBoundingBoxRight: number
+  fontBoundingBoxAscent: number
+  fontBoundingBoxDescent: number
+}
+
 export interface ITextDecoration {
   style?: TextDecorationStyle
 }
