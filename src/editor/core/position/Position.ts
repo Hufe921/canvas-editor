84438303949c9c26f777c41c5ecb151bd3a0dd90
Hufe921 <huangyunfeihufe@hotmail.com>
2024-07-30 20:03:18 +0800

    fix: float image position boundary error #716

--- src/editor/core/position/Position.ts
+++ src/editor/core/position/Position.ts
@@ -643,7 +643,7 @@ export class Position {
     payload: IGetFloatPositionByXYPayload
   ): ICurrentPosition | void {
     const { x, y } = payload
-    const currentPageNo = this.draw.getPageNo()
+    const currentPageNo = payload.pageNo ?? this.draw.getPageNo()
     const currentZone = this.draw.getZone().getZone()
     for (let f = 0; f < this.floatPositionList.length; f++) {
       const {
@@ -658,6 +658,7 @@ export class Position {
         pageNo
       } = this.floatPositionList[f]
       if (
+        currentPageNo === pageNo &&
         element.type === ElementType.IMAGE &&
         element.imgDisplay === payload.imgDisplay &&
         (!floatElementZone || floatElementZone === currentZone)
@@ -667,8 +668,7 @@ export class Position {
           x >= imgFloatPosition.x &&
           x <= imgFloatPosition.x + element.width! &&
           y >= imgFloatPosition.y &&
-          y <= imgFloatPosition.y + element.height! &&
-          +currentPageNo === +pageNo
+          y <= imgFloatPosition.y + element.height!
         ) {
           if (isTable) {
             return {
