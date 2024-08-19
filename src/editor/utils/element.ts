eb7a87b4a8c2144e6c52a21f8502b98a3318c578
yulei <yulei@yinhai.com>
2024-08-19 14:50:43 +0800

    fix: 修复zipElementList中跨页表格结构处理异常问题

--- src/editor/utils/element.ts
+++ src/editor/utils/element.ts
@@ -606,10 +606,10 @@ export function zipElementList(
         ) as Array<Array<ITd | undefined | null>>
         element.trList!.forEach((tr, trIndex) => {
           let tdIndex = 0
-          while (virtualTable[trIndex][tdIndex] === null) {
-            tdIndex++
-          }
           tr.tdList.forEach(td => {
+            while (virtualTable[trIndex][tdIndex] === null) {
+              tdIndex++
+            }
             virtualTable[trIndex][tdIndex] = td
             for (let i = 1; i < td.rowspan; i++) {
               virtualTable[trIndex + i][tdIndex] = null
