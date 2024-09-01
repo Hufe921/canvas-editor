abcdb3800b772e9502ea28e953481c82c0fac97f
Hufe <huangyunfeihufe@hotmail.com>
2024-09-01 13:39:46 +0800

    Update Tr.ts

--- src/editor/interface/table/Tr.ts
+++ src/editor/interface/table/Tr.ts
@@ -2,10 +2,10 @@ import { ITd } from './Td'
 
 export interface ITr {
   id?: string
+  extension?: unknown
+  externalId?: string
   height: number
   tdList: ITd[]
   minHeight?: number
   pagingRepeat?: boolean // 在各页顶端以标题行的形式重复出现
-  extension?: unknown;
-  externalId?: string;
 }
