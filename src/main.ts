ff230df4f2a00c0198e380c5b4f65abde8c065a8
Hufe921 <huangyunfeihufe@hotmail.com>
2025-06-06 22:00:11 +0800

    chore: add row and col position display

--- src/main.ts
+++ src/main.ts
@@ -1741,6 +1741,17 @@ window.onload = function () {
         scrollIntoView(commentDom, activeCommentDom)
       }
     }
+
+    // 行列信息
+    const rangeContext = instance.command.getRangeContext()
+    if (rangeContext) {
+      document.querySelector<HTMLSpanElement>('.row-no')!.innerText = `${
+        rangeContext.startRowNo + 1
+      }`
+      document.querySelector<HTMLSpanElement>('.col-no')!.innerText = `${
+        rangeContext.startColNo + 1
+      }`
+    }
   }
 
   instance.listener.visiblePageNoListChange = function (payload) {
