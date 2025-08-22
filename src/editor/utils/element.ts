79941a17a5c0bb6e4df5a8dff6701ec421c643b6
ANIKET MODKER <aniketmodker24@gmail.com>
2025-08-22 16:27:13 +0530

    fix: table width and alignment issue when using getHTML and setHTML #991
    Co-authored-by: Hufe921 <huangyunfeihufe@hotmail.com>

--- src/editor/utils/element.ts
+++ src/editor/utils/element.ts
@@ -43,6 +43,7 @@ import { ElementType } from '../dataset/enum/Element'
 import { ListStyle, ListType, UlStyle } from '../dataset/enum/List'
 import { RowFlex } from '../dataset/enum/Row'
 import { TableBorder, TdBorder } from '../dataset/enum/table/Table'
+import { VerticalAlign } from '../dataset/enum/VerticalAlign'
 import { DeepRequired } from '../interface/Common'
 import { IControlSelect } from '../interface/Control'
 import { IEditorOption } from '../interface/Editor'
@@ -1553,6 +1554,8 @@ export function getElementListByHTML(
             colgroup: [],
             trList: []
           }
+          // colgroup
+          const colElements = tableElement.querySelectorAll('colgroup col')
           // 基础数据
           tableElement.querySelectorAll('tr').forEach(trElement => {
             const trHeightStr = window
@@ -1571,7 +1574,10 @@ export function getElementListByHTML(
               const td: ITd = {
                 colspan: tableCell.colSpan,
                 rowspan: tableCell.rowSpan,
-                value: valueList
+                value: valueList,
+                verticalAlign: window.getComputedStyle(tdElement)
+                  .verticalAlign as VerticalAlign,
+                width: parseFloat(window.getComputedStyle(tdElement).width)
               }
               if (tableCell.style.backgroundColor) {
                 td.backgroundColor = tableCell.style.backgroundColor
@@ -1588,8 +1594,9 @@ export function getElementListByHTML(
             )
             const width = Math.ceil(options.innerWidth / tdCount)
             for (let i = 0; i < tdCount; i++) {
+              const colElement = colElements[i]?.getAttribute('width')
               element.colgroup!.push({
-                width
+                width: colElement ? parseFloat(colElement) : width
               })
             }
             elementList.push(element)
